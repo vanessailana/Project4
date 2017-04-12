3fda54e94194339ffc5bbcee81cc4ee34dddc242
Vanessa Klotzman <vanessa.klotzman@metalab.csun.edu>
2017-04-12 07:38:24 -0700

    fixed center tags

--- resources/views/pages/welcomeugly.blade.php
+++ resources/views/pages/welcomeugly.blade.php
@@ -12,25 +12,38 @@
 
 <div class="container">
 
+<center>
+
 <h1> Welcome  {{ Auth::user()->username }}! </h1>
 <h1> Remeber We  <kbd> all </kbd> are food critics at heart <i class="fa fa-heart-o" aria-hidden="true"></i>
  </h1>
 
+</center>
  <h1> As part of being an admin you get to judge food </h1>
+ <center>
 
 <iframe width="900" height="345" src="https://www.youtube.com/embed/cx-foylAP40?autoplay=1">
 </iframe>
 
+</center>
+
 
 
 @else 
+<center>
 <h1> Have fun and happy reviewing <i class="fa fa-heart-o" aria-hidden="true"></i>
  </h1>
 
 <h1> Welcome  {{ Auth::user()->username }}! </h1>
 
+</center>
+
+<center>
+
 <iframe width="900" height="345" src="https://www.youtube.com/embed/8Pw8B-0tg5E?autoplay=1">
 </iframe>
+
+</center>
 
 
 
